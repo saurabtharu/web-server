201bb2a6ea09d77613997b00953b853dfa78286f
saurabtharu <saurab.tharu2@gmail.com>
2023-10-13 06:13:05 +0545

    Web Server : Handcoded HTTP response 200 OK

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listner = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
-        println!("The connection established: {:?}", stream);
+        handle_connection(stream);
     }
 }
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    println!("{:?}", response);
+    stream.write_all(response.as_bytes()).unwrap()
+
+    // println!("Request : {:#?}", http_request);
+    // println!("==============================================================================")
+}
+
+/*
+HTTP Request format:
+    Method Request-URI HTTP-Version CRLF
+    HTTP Request format:
+    headers CRLF
+    message-body
+
+HTTP Response format:
+    HTTP-Version Status-Code Reason-Phrase CRLF         // status line
+    headers CRLF                                        //
+    message-body                                        //
+
+e.g.
+
+*/
