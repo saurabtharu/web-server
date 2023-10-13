1e6d401dbca77b1af8f44ec60ce81be728ff31d0
saurabtharu <saurab.tharu2@gmail.com>
2023-10-13 18:59:09 +0545

    Web Server : sending requests to threads via Channels

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 /// `ThreadPool` data structure to hold a vector of `Worker` instances
 ///
 /// field: workers -> Vec<Worker>
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -19,12 +25,18 @@ impl ThreadPool {
     /// use the `for` loop counter to generate an `id`, create a new `Worker` with that `id`, and store the worker in the vector
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
+
         for id in 0..size {
             // create some threads and store them in the vector
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        Self { workers }
+        Self { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -44,8 +56,10 @@ struct Worker {
 
 /// function that takes an id number and returns a Worker instance that holds the id and a thread spawned with an empty closure
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
         Worker { id, thread }
     }
 }
