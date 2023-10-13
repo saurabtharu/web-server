9608d4fb226c5f11c410e6a259b87673f4118fb7
saurabtharu <saurab.tharu2@gmail.com>
2023-10-13 18:39:44 +0545

    Web Server : `Worker` struct to send the code from `ThreadPool` to a `Thread`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use std::thread;
 
+/// `ThreadPool` data structure to hold a vector of `Worker` instances
+///
+/// field: workers -> Vec<Worker>
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -12,13 +15,16 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new function panics if the size is zero`
+    ///
+    /// use the `for` loop counter to generate an `id`, create a new `Worker` with that `id`, and store the worker in the vector
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
             // create some threads and store them in the vector
+            workers.push(Worker::new(id));
         }
-        Self { threads }
+        Self { workers }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -27,5 +33,19 @@ impl ThreadPool {
         /* We still use the () after FnOnce because this FnOnce represents a closure that takes no
          * parameters and returns the unit type () */
     {
+    }
+}
+
+/// `Worker` data structure holds an `id` and a `JoinHandle<()>`
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+/// function that takes an id number and returns a Worker instance that holds the id and a thread spawned with an empty closure
+impl Worker {
+    fn new(id: usize) -> Self {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
     }
 }
