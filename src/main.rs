431b2e931547da4869f1e39becd5409be1d84625
saurabtharu <saurab.tharu2@gmail.com>
2023-10-13 06:49:16 +0545

    Web Server : refactored the redundant code and Single threaded Simple web server is completed This server serve "pages/hello.html" if the request is to http://127.0.0.1/ else it serve "pages/404.html"

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,6 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    if http_request[0] == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("pages/hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap()
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("pages/404.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap()
-    }
-
-    // println!("Request : {:#?}", http_request);
-    // println!("==============================================================================")
-}
-
 /*
 HTTP Request format:
     Method Request-URI HTTP-Version CRLF
@@ -58,3 +28,25 @@ HTTP Response format:
 e.g.
 
 */
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let (status_line, filename) = if http_request[0] == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "pages/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "pages/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap()
+}
