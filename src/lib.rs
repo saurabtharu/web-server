9c227b916de4723e03d54235a074354b09f7faff
saurabtharu <saurab.tharu2@gmail.com>
2023-10-13 18:25:58 +0545

    Web Server : creating space to store threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -10,7 +14,11 @@ impl ThreadPool {
     /// The `new function panics if the size is zero`
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        Self
+        let mut threads = Vec::with_capacity(size);
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+        Self { threads }
     }
 
     pub fn execute<F>(&self, f: F)
